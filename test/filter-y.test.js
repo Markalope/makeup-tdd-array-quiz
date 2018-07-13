96a814103f23a0eac04b5cfc55f5dc50cbfa8890
Mark Myers <markalope@gmail.com>
2018-07-13 10:54:46 -0700

    fixed linting error

--- test/filter-y.test.js
+++ test/filter-y.test.js
@@ -2,11 +2,11 @@ const assert = require('assert');
 const filterY = require('../lib/filterY');
 
 describe('Array function', () => {
-    let intial = ['sky', 'hot', 'Yeti', 'green'];
+    let initial = ['sky', 'hot', 'Yeti', 'green'];
     let filtered = ['Is it hot?', 'Is it green?'];
 
     it('filters out words containing the letter Y', () => {
-        const result = filterY(intial);
+        const result = filterY(initial);
         assert.deepEqual(result, ['hot', 'green']);
     });
     

82799caaa243da1fcaa5e9dea3a0f31be26d68c4
Mark Myers <markalope@gmail.com>
2018-07-13 10:54:12 -0700

    wrote test files

---
+++ test/filter-y.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const filterY = require('../lib/filterY');
+
+describe('Array function', () => {
+    let intial = ['sky', 'hot', 'Yeti', 'green'];
+    let filtered = ['Is it hot?', 'Is it green?'];
+
+    it('filters out words containing the letter Y', () => {
+        const result = filterY(intial);
+        assert.deepEqual(result, ['hot', 'green']);
+    });
+    
+    it('forms a sentence with items in the array that do not contain the letter Y', () => {
+        const result = filterY(filtered);
+        assert.deepEqual(result, ['Is it hot?', 'Is it green?']);
+    });
+
+
+});
